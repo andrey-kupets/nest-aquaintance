df0a4196038070b2bdf872d508b61bf5b18e2906
andrey-kupets <andrey.kupets@i.ua>
2022-07-30 23:36:47 +0300

    add new methods (del, update)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UserController {
@@ -23,7 +24,14 @@ export class UserController {
     return this.userService.createUser(user);
   }
 
-  // @Delete('/:id')
-  // @Patch('/:id')
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    this.userService.deleteUser(id);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() user: UpdateUserDto) {
+    return this.userService.updateUser(id, user);
+  }
 }
 
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -23,6 +23,17 @@ export class UserService {
     return user;
   }
 
-  // updateUser(data: UpdateUserDto)
-  // deleteUser(id: string)
+  deleteUser(id: string) {
+    const user = this.users.find((user) => user.id == id);
+    const index = this.users.indexOf(user);
+    this.users.splice(index, 1);
+  }
+
+  updateUser(id: string, data: UpdateUserDto) {
+    const user = this.users.find((user) => user.id == id);
+    const index = this.users.indexOf(user);
+    const updatedUser = {...data, id: user.id };
+    this.users.splice(index, 1, updatedUser);
+    return updatedUser;
+  }
 }
