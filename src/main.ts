f04ba8a6b0112d4e8b87e686044183389a935acd
andrey-kupets <andrey.kupets@i.ua>
2022-08-07 21:55:55 +0300

    swagger: beginning

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import * as fs from 'fs';
+import { join } from 'path';
+
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerDescription = fs.readdirSync(join(__dirname, '..', 'description.markdown'));
+  const config = new DocumentBuilder()
+    .setTitle('Nest for the first')
+    .setDescription(swaggerDescription.toString())
+    .setVersion('1.0')
+    .addTag('nestFest')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,11 +8,14 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
+import { ApiBody, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
+
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from '../auth/jwt-auth.guard';
 
+@ApiTags('Users')
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -24,6 +27,10 @@ export class UserController {
   }
 
   @Get('/:id')
+  // @Get('/:userId/posts/:postId')
+  @ApiQuery({ name: 'id', type: 'string'})
+  // @ApiQuery({ name: 'userId/', type: 'string'})
+  // @ApiQuery({ name: 'postId/', type: 'string'})
   @UseGuards(AuthGuard)
   getOneUser(@Param('id') id: string) {
     // getOneUser(@Param() param: any) {
@@ -32,6 +39,22 @@ export class UserController {
   }
 
   @Post()
+  @ApiBody({ type: CreateUserDto })
+  @ApiOkResponse({
+    status: 200,
+    schema: {
+      example: {
+        id: "62eefb964a1d26ce499f4ab0",
+        name: "AAAAAA",
+        age: 21,
+        email: "andrii@gmail.com",
+        password: "$2a$08$y9nTrB5V1e37FUfivdenz.7kF3eQCQEscjp.QvEkLYUC1wKSWgCk6",
+        status: false,
+        createdAt: "2022-08-06T23:39:02.000Z",
+        updatedAt: "2022-08-06T23:39:02.000Z"
+      }
+    }
+  })
   createUser(@Body() user: CreateUserDto) {
     return this.userService.createUser(user);
   }
